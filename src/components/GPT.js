510bd1752e0d7cd444b106cf1b9e2b5864d5ea64
Gabriel Sarti <gabrielbsarti@gmail.com>
2023-05-28 11:51:36 -0300

    removed tweet url

--- src/components/GPT.js
+++ src/components/GPT.js
@@ -38,7 +38,7 @@ export default function GPT({city, temp}) {
           {textGenerated}
           <div className={styles.tweet}>
             <TwitterShareButton
-              url={'https://clima-tweet-ai.vercel.app/'}
+              url={'https://clima-gpt.vercel.app/'}
               options={{ text: `${textGenerated}`, via: 'ClimaGPT', size: 'large' }}
             />
           </div>
