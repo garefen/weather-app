c1d3fd71291d6ae0b773694d3473b4e032f9a71c
Gabriel Sarti <gabrielbsarti@gmail.com>
2023-05-28 11:49:53 -0300

    added adsense

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,11 +4,8 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }) {
   return (
     <>
-      {/* <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9117287205580830"
-        crossorigin="anonymous"
-      ></Script> */}
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9117287205580830"
+        crossorigin="anonymous"></Script>
       <Component {...pageProps} />
     </>
   )
